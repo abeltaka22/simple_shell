674722eb3478cf213706f6c11723299e75fdf228
informeddeno <informeddeno@gmail.com>
2023-07-28 00:37:58 -0700

    simple shell

--- Simple_shell_0.1.c
+++ Simple_shell_0.1.c
@@ -13,11 +13,11 @@
 */
 char *read_line()
 {
-  char *line = NULL;
-  size_t bufsize = 0;
+char *line = NULL;
+size_t bufsize = 0;
 
-  getline(&line, &bufsize, stdin);
-  return (line);
+getline(&line, &bufsize, stdin);
+return (line);
 }
 /**
 *parse_line- function to parse the input line and split it into tokens
@@ -26,34 +26,34 @@ char *read_line()
 */
 char **parse_line(char *line)
 {
-  int bufsize = BUFFER_SIZE, position = 0;
-  char **tokens = malloc(bufsize * sizeof(char *));
-  char *token;
+int bufsize = BUFFER_SIZE, position = 0;
+char **tokens = malloc(bufsize * sizeof(char *));
+char *token;
 
-  if (!tokens)
-    {
-      perror("Memory allocation error");
-      exit(EXIT_FAILURE);
-    }
-  token = strtok(line, "\t\n\r\a");
-  while (token != NULL)
-    {
-      tokens[position] = token;
-      position++;
-      if (position >= bufsize)
-	{
-	  bufsize += BUFFER_SIZE;
-	  tokens = realloc(tokens, bufsize * sizeof(char *));
-	  if (!tokens)
-	    {
-	      perror("Memory allocation error");
-	      exit(EXIT_FAILURE);
-	    }
-	}
-      token = strtok(NULL, "\t\n\r\a");
-    }
-  tokens[position] = NULL;
-  return (tokens);
+if (!tokens)
+{
+perror("Memory allocation error");
+exit(EXIT_FAILURE);
+}
+token = strtok(line, "\t\n\r\a");
+while (token != NULL)
+{
+tokens[position] = token;
+position++;
+if (position >= bufsize)
+{
+bufsize += BUFFER_SIZE;
+tokens = realloc(tokens, bufsize *sizeof(char *));
+if (!tokens)
+{
+perror("Memory allocation error");
+exit(EXIT_FAILURE);
+}
+}
+token = strtok(NULL, "\t\n\r\a");
+}
+tokens[position] = NULL;
+return (tokens);
 }
 
 /**
@@ -63,25 +63,26 @@ char **parse_line(char *line)
 */
 int execute_builtin(char **args)
 {
-  if (strcmp(args[0], "cd") == 0)
-    {
-      if (args[1] == NULL)
-	{
-	  fprintf(stderr, "cd: expected argument to \"cd\"\n");
-	} else
-	{
-	  if (chdir(args[1]) != 0)
-	    {
-	      perror("cd");
-	    }
-	}
-      return (0);
-    }
-  else if (strcmp(args[0], "exit") == 0)
-    {
-      return (0);
-    }
-  return (-1);
+if (strcmp(args[0], "cd") == 0)
+{
+if (args[1] == NULL)
+{
+fprintf(stderr, "cd: expected argument to \"cd\"\n");
+}
+else
+{
+if (chdir(args[1]) != 0)
+{
+perror("cd");
+}
+}
+return (0);
+}
+else if (strcmp(args[0], "exit") == 0)
+{
+return (0);
+}
+return (-1);
 }
 
 /**
@@ -91,39 +92,37 @@ int execute_builtin(char **args)
 */
 int execute_command(char **args)
 {
-  pid_t pid;
-  int status;
+pid_t pid;
+int status;
 
-  pid = fork();
+pid = fork();
 
-  if (pid == 0)
-    {
-      char *command = args[0];
-      char **argv = (char **)malloc(2 * sizeof(char *));
+if (pid == 0)
+{
+char *command = args[0];
+char **argv = (char **)malloc(2 * sizeof(char *));
 
-      argv[0] = command;
-      argv[1] = NULL;
+argv[0] = command;
+argv[1] = NULL;
 
-      if (execvp(command, argv) == -1)
-	{
-	  perror("Error");
-	  free(argv);
-	  exit(EXIT_FAILURE);
-	}
-    }
-  else if (pid < 0)
-    {
-      perror("Error");
-    }
-  else
-    {
-      do {
-	  waitpid(pid, &status, WUNTRACED);
-	}
-      while
-	(!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
-  return (1);
+if (execvp(command, argv) == -1)
+{
+perror("Error");
+free(argv);
+exit(EXIT_FAILURE);
+}
+}
+else if (pid < 0)
+{
+perror("Error");
+}
+else
+{
+do {
+waitpid(pid, &status, WUNTRACED);
+} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+}
+return (1);
 }
 /**
  * main - Entry point of the program.
@@ -132,33 +131,32 @@ int execute_command(char **args)
  */
 int main(void)
 {
-  char *line;
-  char **args;
-  int status;
+char *line;
+char **args;
+int status;
 
-  status = 1;
+status = 1;
 
-  do {
-      printf("($) ");
-      line = read_line();
-      args = parse_line(line);
+do {
+printf("($) ");
+line = read_line();
+args = parse_line(line);
 
-      if (args[0] != NULL)
-	{
-	  int builtin_status = execute_builtin(args);
+if (args[0] != NULL)
+{
+int builtin_status = execute_builtin(args);
 
-	  if (builtin_status == -1)
-	    {
-	      status = execute_command(args);
-	    }
-	  else if (builtin_status == 0)
-	    {
-	      status = 0;
-	    }
-	}
-      free(line);
-      free(args);
-    }
-  while (status);
-  return (0);
+if (builtin_status == -1)
+{
+status = execute_command(args);
+}
+else if (builtin_status == 0)
+{
+status = 0;
+}
+}
+free(line);
+free(args);
+} while (status);
+return (0);
 }
